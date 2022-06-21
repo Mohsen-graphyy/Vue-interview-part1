ae316a685d901f617e848be892058b099e4834d8
mohsen_graphyy <piri.mohsen@ut.ac.ir>
2022-06-21 21:36:32 +0430

    Debug comment issue

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div
+    class="
+      bg-gradient-to-r
+      from-violet-400
+      to-blue-400
+      flex
+      justify-center
+      p-5
+      md:p-10
+      items-center
+    "
+  >
+    <div
+      class="
+        container
+        mx-auto
+        px-5
+        md:px-24
+        py-10
+        rounded-lg
+        bg-white bg-opacity-20
+      "
+    >
+      <div class="text-center font-bold text-3xl text-white">Profiles List</div>
+      <div class="">
+        <div class="flex flex-row gap-5 justify-between items-center my-10">
+          <button class="basis-1/2 p-3 bg-white rounded-lg" @click="sortAsc">
+            ▲
+          </button>
+          <button class="basis-1/2 p-3 bg-white rounded-lg" @click="sortDesc">
+            ▼
+          </button>
+        </div>
+
+        <ProfileCard
+          v-for="profile in profiles"
+          :key="profile.id"
+          :profile="profile"
+          class="profile"
+        />
+      </div>
+      <p class="text-white font-bold">Edit by Mohsen Piri</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from "./components/ProfileCard";
+
+export default {
+  name: "App",
+
+  components: {
+    ProfileCard,
+  },
+
+  data() {
+    return {
+      profiles: [
+        {
+          id: 1,
+          name: "Wojciech",
+          email: "wojciech@poz.pl",
+          description: "Anaesthesiologist",
+          likes: 34,
+        },
+        {
+          id: 2,
+          name: "Maria",
+          email: "maria@poz.pl",
+          description: "Radiologist",
+          likes: 28,
+        },
+        {
+          id: 3,
+          name: "Anna",
+          email: "anna@poz.pl",
+          description: "Surgeon",
+          likes: 53,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    sortAsc() {
+      this.profiles.sort(function (a, b) {
+        return a.likes - b.likes;
+      });
+    },
+
+    sortDesc() {
+      this.profiles.sort(function (a, b) {
+        return b.likes - a.likes;
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bg-white my-5 p-5 rounded-lg">
+    <div class="card">
+      <div class="flex flex-row justify-start gap-2 items-center">
+        <DoctorIcon class="w-16 rounded-full bg-violet-200" />
+        <div class="flex flex-col">
+          <strong>{{ profile.name }}</strong>
+          <a :href="profile.email" class="email">{{ profile.email }}</a>
+        </div>
+      </div>
+      <div class="text-violet-500 my-2">
+        {{ profile.description }}
+      </div>
+      <div class="flex flex-row justify-start items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5 text-sky-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <p class="ml-2">{{ profile.likes }}</p>
+      </div>
+    </div>
+    <div class="comment">
+      <input class="comment-input" placeholder="Write your comment..." />
+    </div>
+  </div>
+</template>
+
+<script>
+import DoctorIcon from "./DoctorIcon";
+
+export default {
+  name: "ProfileCard",
+
+  components: {
+    DoctorIcon,
+  },
+
+  props: {
+    profile: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
